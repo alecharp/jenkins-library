d21c213f29a99f9ec3b8d19beb07ef7e387b85b3
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2016-10-07 22:04:25 +0200

    Describe how to build Docker image

---
+++ vars/buildDockerImg.groovy
@@ -0,0 +1,25 @@
+#!groovy
+
+def call(def imageName) {
+  properties([
+    buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')),
+    pipelineTriggers([[$class: 'PeriodicFolderTrigger', interval: '2m']])
+  ])
+
+  node {
+    stage('Checkout') {
+      checkout scm
+      sh 'git rev-parse --short HEAD > GIT_COMMIT'
+      short_commit = readFile('GIT_COMMIT').trim()
+      currentBuild.description = "${short_commit}"
+    }
+
+    stage('Build') {
+      image = docker.build("${imageName}:${short_commit}")
+    }
+
+    stage('Publish') {
+      image.push()
+    }
+  }
+}
