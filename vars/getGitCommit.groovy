58fd5a0800fa2605bc0f310aef5d1fe2fa3565ca
Adrien Lecharpentier <adrien.lecharpentier@gmail.com>
2016-10-25 10:08:52 +0200

    Simplify Git commmit discovery

--- vars/getGitCommit.groovy
+++ vars/getGitCommit.groovy
@@ -1,7 +1,5 @@
 #!groovy
 
 def call() {
-  sh 'git rev-parse --short HEAD > GIT_COMMIT'
-  def shortCommit = readFile('GIT_COMMIT').trim()
-  return shortCommit
+  return sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
 }
